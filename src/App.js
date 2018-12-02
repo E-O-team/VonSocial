2ebd6f0245b9e6a1d7ca49a202e5cef7bbaf1c5c
KNVN <tkien2703@gmail.com>
2018-12-02 14:53:46 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
-import Todos from './components/Todos';
+import Home from './components/Home';
 import Login from './components/Login';
 import { Provider } from 'react-redux';
 import store from './store';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
          <Provider store={store}>
-             <div className="App">
-               <Login/>
-             </div>
+          <Router>
+             // <div className="App">
+             //   <Login/>
+             // </div>
+             <Route path='/' component={Home} />
+             <Route path='/login' component={Login} />
+
+          </Router>
          </Provider>
     );
   }
